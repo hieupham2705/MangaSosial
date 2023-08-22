c68607ef66b62a86d85ce143b695a36f94250b41
HungVu12112 <hungmno123@gmail.com>
2023-08-22 12:06:35 +0700

    22/08

--- app/src/main/java/com/example/mangasosical/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/example/mangasosical/ui/register/RegisterFragment.kt
@@ -1,6 +1,7 @@
 package com.example.mangasosical.ui.register
 
 import android.widget.Toast
+import androidx.lifecycle.ViewModel
 import com.example.mangasosical.base.BaseFragment
 import com.example.mangasosical.base.BaseViewModel
 import com.example.mangasosical.data.service.ApiService
@@ -8,8 +9,8 @@ import com.example.mangasosical.databinding.FragmentRegisterBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class RegisterFragment : BaseFragment<FragmentRegisterBinding>(FragmentRegisterBinding::inflate) {
-
-    override val viewModel: BaseViewModel
+     override val viewModel by viewModel<RegisterViewModel>()
+    private val mviewModel: BaseViewModel
         get() = BaseViewModel()
     override fun initData() {
         viewModel.apply {
@@ -30,7 +31,8 @@ class RegisterFragment : BaseFragment<FragmentRegisterBinding>(FragmentRegisterB
                     Toast.makeText(context,"Không được bổ trống",Toast.LENGTH_LONG).show()
                 }
                 else {
-                    ////
+                    // đoạn này là phần call
+                    viewModel.CallApiRegister(email,password)
                 }
             }
         }
